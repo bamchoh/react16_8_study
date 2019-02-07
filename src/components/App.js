a4326d3783fff7d132acc986b74f9fc04027811e
bamchoh <bamchoh@gmail.com>
2019-02-07 21:32:55 +0900

    Add useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
 
+function getRandomInit(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function Counter({initialCount}) {
+  const [count, setCount] = useState(initialCount);
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </>
+  );
+}
+
 export default () => {
   const [a, b] = useState(0);
   return (
-    <div>
-      <button onClick={() => { b(a + 1); }}>
-        \ovo/ {"<"}{a} times!!
-      </button>
-    </div>
+    <Counter initialCount={() => {
+      return getRandomInit(100)
+    }} />
   )
 };
