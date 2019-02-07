24ad844ae262dbbbba85cb6573ce5336a835c207
bamchoh <bamchoh@gmail.com>
2019-02-07 22:51:11 +0900

    useReducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,32 @@
 import * as React from 'react';
-const { useState, useEffect } = React;
+const { useReducer } = React;
 
-export default () => {
-  const [a, b] = useState(0);
+const initialState = {count: 0};
 
-  useEffect(() => {
-    const timerid = setTimeout(() => {
-      console.log("triggered!!");
-    }, 1000);
-    console.log("timer");
+function reducer(state, action) {
+  switch(action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+  }
+}
 
-    return () => {
-      clearTimeout(timerid);
-      console.log("clean up useEffect")
-    }
-  }, [a]);
-
+function Counter({initialCount}) {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
-      time: <b>{a}</b>
-      <button onClick={() => b(a+1)}>+</button>
+      Count: {state.count}
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
     </>
-  )
+  );
+}
+
+export default () => {
+  return (
+    <Counter initialCount={10} />
+  );
 };
